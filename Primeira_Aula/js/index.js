65e8e887ba76bcc1a89e19db159ddede887b05b1
Pablo Assis <52177225+PabloJr84@users.noreply.github.com>
2023-07-05 15:50:23 -0300

    3ª Atualização JS Aula e video

--- Primeira_Aula/js/index.js
+++ Primeira_Aula/js/index.js
@@ -1,1 +1,7 @@
-document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>";
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>";  
+console.log("Oi isso é um console.log");
+
+var a = 5;
+var b = 1;
+var c = a + b;
+console.log(c);
